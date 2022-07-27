286560e199d51c32408ac0cd356fc5e8d398ea6c
Kim Jisena <pjohmaths@gmail.com>
2022-07-27 23:58:41 +0300

    add clock control buttons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import {GrPowerReset} from 'react-icons/gr';
+import {GrPlay} from 'react-icons/gr';
+import {GrPause} from 'react-icons/gr';
+import {VscRefresh} from 'react-icons/vsc';
+import {VscDebugPause} from 'react-icons/vsc';
+import {VscDebugStart} from 'react-icons/vsc';
+import {VscDebugRestart} from 'react-icons/vsc';
 
 function Timer() {
   return (
     <>
         {/*timer-label and time-left goes into the clock container */}
-        <div id='clock'>
-            <div id='timer-label'>Session</div>
-            <div id='time-left'>25:00</div>
+        <div id='clock' className={`relative w-[50%] flex flex-col items-center h-[75%] bg-red rounded-full`}>
+            <div id='clock-labels' className={`absolute bottom-[15%] flex flex-col items-center w-[70%] h-[35%] text-black bg-white`}>
+                <div id='timer-label' className={`text-xl font-font-one font-bold`}>Session</div>
+                <div id='time-left' className={`text-3xl font-font-two font-bold`}>25:00</div>
+            </div>
         </div>
 
         {/**start_stop and reset goes into the controls container */}
-        <div id='controls'>
-            <div id='start_stop'></div>
-            <div id='reset'></div>
+        <div id='controls' className={`w-[40%] h-[20%] flex justify-evenly items-center`}>
+
+            <div id='start_stop' className={`w-8 h-8 flex justify-center items-center bg-green rounded-md`}>
+                <VscDebugStart className={`text-black`}  size={`24px`} />
+            </div>
+
+            <div id='reset' className={`w-8 h-8 flex justify-center items-center bg-green rounded-md`}>
+                <VscDebugRestart className={`text-black`}  size={`24px`} />
+            </div>
         </div>
     </>
   );
