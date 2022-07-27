108c9904aa6349d1009c20126f0ab84899b84f08
Kim Jisena <pjohmaths@gmail.com>
2022-07-27 22:23:36 +0300

    add colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
       ],
   theme: {
+    colors: {
+      'white': '#F9FAFE',
+      'black': '#444549',
+      'red': '#F73349',
+      'green': '#01D86D',
+      'orange': '#C56F17'
+    },
+    fontFamily: {
+      'font-one': ['Montserrat', 'sans-serif'],
+      'font-two': ['Quicksand', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
