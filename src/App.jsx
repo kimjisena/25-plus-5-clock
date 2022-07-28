27dddb9edfddd74faec23a2722acd09e43d9d007
Kim Jisena <pjohmaths@gmail.com>
2022-07-28 09:35:04 +0300

    implement countdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React, {useState} from 'react';
-import { act } from 'react-dom/test-utils';
 import GenericLabel from './components/GenericLabel';
 import Timer from './components/Timer';
 
 
+let countId;
+
 function App() {
     const [breakLength, setBreakLength] = useState(5);
     const [sessionLength, setSessionLength] = useState(25);
+    const [timeLeft, setTimeLeft] = useState(['25', '00']);
+    // const [countId, setCountId] = useState(null);
+
+    // const [minutes, setMinutes] = useState(8);
+    // const [seconds, setSeconds] = useState(3);
+
+    const [counting, setCounting] = useState(false);
+    const [session, setSession] = useState(true);
 
     const incrementDuration = (duration) => {
         if (duration === 'break') {
             setBreakLength(breakLength + 1);
         } else {
+            setTimeLeft([`${sessionLength + 1}`, '00']);
             setSessionLength(sessionLength + 1);
         }
     };
@@ -20,6 +30,7 @@ function App() {
         if (duration === 'break') {
             setBreakLength(breakLength - 1);
         } else {
+            setTimeLeft([`${sessionLength - 1}`, '00']);
             setSessionLength(sessionLength - 1);
         }
     }
@@ -49,6 +60,59 @@ function App() {
         }
     }
 
+    const switchTimer = () => {
+
+    }
+
+    let mins, secs;
+    mins = Number(timeLeft[0]);
+    secs = Number(timeLeft[1]);
+
+    const countdown = () => {
+
+        if (secs > 0) { // we have seconds
+            secs--;
+            console.log(`Time: ${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`);
+            setTimeLeft([`${mins < 10 ? `0${mins}` : mins}`, `${secs < 10 ? `0${secs}` : secs}`]);
+        } else {
+            if (mins > 0) { // we have minutes
+                mins--;
+                secs = 59;
+                console.log(`Time: ${mins}:${secs}`);
+                setTimeLeft([`${mins < 10 ? `0${mins}` : mins}`, `${secs}`]);
+            } else {
+                console.log(`Time is over: ${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`);
+                clearInterval(countId); // don't call it again
+                setCounting(false); // stop counting
+                //setCountId(null); // i mean it
+            }
+        }
+    }
+
+
+    const handleTimer = (event) => {
+        let control = event.currentTarget.id;
+        if (control === 'reset') {
+            if (counting) { // stop any counter
+                clearInterval(countId); 
+                setCounting(false);
+            }
+            setBreakLength(5);
+            setSessionLength(25);
+            setTimeLeft(['25', '00']);
+            
+        } else if (control === 'start_stop') {
+            console.log('start or stop');
+            if (counting) { // stop counting
+                clearInterval(countId); 
+                setCounting(false);
+            } else { // start counting
+                setCounting(true);
+                countId = setInterval(countdown, 1000);
+            }
+        }
+    }
+
     return (
         <div className={`w-full h-[100vh] flex justify-center items-center`}>
             <div className={`wrapper flex flex-col justify-center items-center w-96 h-96 bg-white border-8 border-black rounded-3xl shadow-black shadow-inner`}>
@@ -57,7 +121,7 @@ function App() {
                     <GenericLabel name={`Session`} length={sessionLength} clickHandler={handleDurationLength} />
                 </div>
                 <div className={`timer-wrapper w-full h-2/3 flex flex-col justify-evenly items-center`}>
-                    <Timer />
+                    <Timer clickHandler={handleTimer} timeLeft={timeLeft.join(':')} counting={counting} />
                 </div>
             </div>
         </div>
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,7 @@ import {VscDebugPause} from 'react-icons/vsc';
 import {VscDebugStart} from 'react-icons/vsc';
 import {VscDebugRestart} from 'react-icons/vsc';
 
-function Timer() {
+function Timer({clickHandler, timeLeft, counting}) {
   return (
     <>
         {/*timer-label and time-left goes into the clock container */}
@@ -16,18 +16,18 @@ function Timer() {
 
             <div id='clock-labels' className={`absolute bottom-[30%] flex flex-col items-center w-[70%] h-[40%%] text-black`}>
                 <div id='timer-label' className={`text-xl font-font-one font-bold`}>Session</div>
-                <div id='time-left' className={`text-5xl font-font-two font-bold`}>25:00</div>
+                <div id='time-left' className={`text-5xl font-font-two font-bold`}>{timeLeft}</div>
             </div>
         </div>
 
         {/**start_stop and reset goes into the controls container */}
         <div id='controls' className={`w-[40%] h-[20%] flex justify-evenly items-center`}>
 
-            <div id='start_stop' className={`w-8 h-8 flex justify-center items-center bg-green rounded-md hover:cursor-pointer shadow-black active:shadow-inner shadow-sm`}>
-                <VscDebugStart className={`text-black`}  size={`24px`} />
+            <div id='start_stop' onClick={(ev) => clickHandler(ev)} className={`w-8 h-8 flex justify-center items-center bg-green rounded-md hover:cursor-pointer shadow-black active:shadow-inner shadow-sm`}>
+                {counting ? <VscDebugPause className={`text-black`}  size={`24px`} /> : <VscDebugStart className={`text-black`}  size={`24px`} />}
             </div>
 
-            <div id='reset' className={`w-8 h-8 flex justify-center items-center bg-green rounded-md hover:cursor-pointer shadow-black active:shadow-inner shadow-sm`}>
+            <div id='reset' onClick={(ev) => clickHandler(ev)} className={`w-8 h-8 flex justify-center items-center bg-green rounded-md hover:cursor-pointer shadow-black active:shadow-inner shadow-sm`}>
                 <VscDebugRestart className={`text-black`}  size={`24px`} />
             </div>
         </div>
